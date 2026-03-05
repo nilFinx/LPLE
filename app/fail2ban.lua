f640639f0b9d2ec5bdfb623981d9331a86be4b16
nilfinx <nulflox@disroot.org>
2026-03-05 23:38:58 +0900

    Better fail2ban debug logging

--- app/fail2ban.lua
+++ app/fail2ban.lua
@@ -4,12 +4,17 @@ BannedIPs = {}
 
 local max_allowed = Config.secure.fail2ban_max_tries
 
-function AddIP(ip)
+function AddIP(ip, req)
 	local lip = leaderboard[ip]
 	leaderboard[ip] = lip and lip + 1 or 1
+	l:info(ip.." failed auth")
 	if (lip or 1) >= max_allowed - 1 then
 		BannedIPs[ip] = true
 		l:info("Banned %s for failing fail2ban", ip)
+		if req and Config.log_level == "debug" then
+			l:debug("Headers:")
+			p(req.headers)
+		end
 	end
 end
 
