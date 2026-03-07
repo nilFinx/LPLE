e42f24944858f831c9374fd7ed0a10792207177b
nilfinx <nulflox@disroot.org>
2026-03-07 17:47:04 +0900

    Just better code

--- app/uvproxy.lua
+++ app/uvproxy.lua
@@ -1,22 +1,21 @@
-return function(cSocket, sSocket)
-	sSocket:read_start(function(err, chunk)
+local gc = collectgarbage
+
+local rs = function(s1, s2)
+	s1:read_start(function(err, chunk)
 		if err then
 			l:error("Upstream error: "..err)
-			cSocket:close_reset()
+			s2:close_reset()
 		elseif not chunk then
-			cSocket:shutdown()
+			s2:shutdown(function()
+				s2:close()
+				gc()
+			end)
 		else
-			cSocket:write(chunk)
+			s2:write(chunk)
 		end
 	end)
-	cSocket:read_start(function(err, chunk)
-		if err then
-			l:error("Client error: "..err)
-			sSocket:close_reset()
-		elseif not chunk then
-			sSocket:shutdown()
-		else
-			sSocket:write(chunk)
-		end
-	end)
+end
+
+return function(s1, s2)
+	rs(s1, s2) rs(s2, s1)
 end
