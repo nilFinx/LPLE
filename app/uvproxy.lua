1f8696e614dd37b83a56430d48bd9bd2866f2cf6
nilFinx <nulflox@disroot.org>
2026-05-01 20:55:59 +0900

    Better GC?

--- app/uvproxy.lua
+++ app/uvproxy.lua
@@ -4,10 +4,14 @@ local rs = function(s1, s2)
 	s1:read_start(function(err, chunk)
 		if err then
 			l:error("Upstream error: "..err)
-			s2:close_reset()
+			s2:close_reset(function()
+				s1 = nil; s2 = nil
+				gc()
+			end)
 		elseif not chunk then
 			s2:shutdown(function()
 				s2:close()
+				s1 = nil; s2 = nil
 				gc()
 			end)
 		else
