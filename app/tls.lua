64064fd64d102613addc1344ed0412e4d5a6ef38
nilfinx <nulflox@disroot.org>
2026-05-01 10:56:14 +0900

    TLS version parsing belongs here

--- app/tls.lua
+++ app/tls.lua
@@ -13,3 +13,29 @@ X_CIPHERS =
 	"ECDHE-RSA-AES128-SHA:"..
 	"ECDHE-RSA-AES256-SHA"..
 	"@SECLEVEL=0" -- Allow TLSv1.1, 1.0, etc
+
+-- 0.3, 1.0, 1.1, 1.2, 1.3
+-- Expects SSLv3, TLSv1.3, etc
+function Ver2Num(ver)
+	if ver:sub(1,4) == "TLSv" then
+		return tonumber(ver:sub(5))
+	else -- SSL
+		return tonumber("0."..ver:sub(5))
+	end
+end
+
+local minver = Ver2Num((Config.secure.tls.min))
+local maxver = Ver2Num((Config.secure.tls.max))
+
+---@param v number 0.3, 1.0 ~ 1.3
+---@return boolean fail
+---@return "L"|"H"? reason L=lower than expected, H=higher than expected
+function CheckVer(v)
+	if v < minver then
+		return true, "L"
+	end
+	if v > maxver then
+		return true, "H"
+	end
+	return false
+end
