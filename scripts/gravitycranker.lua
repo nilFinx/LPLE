c0f5770a3220b5de709da26d3b7fc46da16cf7e7
nilfinx <nulflox@disroot.org>
2026-05-05 11:34:04 +0900

    Adapt for coro-http

--- scripts/gravitycranker.lua
+++ scripts/gravitycranker.lua
@@ -2,43 +2,47 @@
 local lu = require "ext.lua-url"
 local json = require "json"
 
----@param req luvit.http.IncomingMessage
----@param res luvit.http.ServerResponse
-return nil, {["gravitybox.ceco.sk.eu.org"] = function(req, res)
-	if req.url == "/service.php" then
-		local body = ""
-		req:on('data', function (chunk)
-			body = body .. chunk
-		end)
-		req:on("end", function()
-			if not (body and body ~= "") then
-				res.statusCode = 400
-			end
-			local q = lu.parseArgs(body)
-			-- Has to be caps for some reason.
-			if q and q.transactionId and string.upper(q.transactionId) == "YES" then
-				res.statusCode = 200
-				res:setHeader("Content-Type", "application/json")
-				---@diagnostic disable-next-line: param-type-mismatch
-				res:finish(json.encode({
-					message = "henlo :3",
-					status = "OK",
-					-- TRANSACTION_VALID, TRANSACTION_INVALID, TRANSACTION_VIOLATION, TRANSACTION_BLOCKED
-					trans_status = "TRANSACTION_VALID"
-				}))
-			else
-				res.statusCode = 400
-				res:setHeader("Content-Type", "application/json")
-				---@diagnostic disable-next-line: param-type-mismatch
-				res:finish(json.encode({
-					message = "henlo :3",
-					status = "OK",
-					trans_status = "TRANSACTION_INVALID"
-				}))
-			end
-		end)
+---@param req ch-request
+---@return coro-http.alias.response
+---@return string? body
+return {nil, {["gravitybox.ceco.sk.eu.org"] = function(req, body)
+	if req.path == "/service.php" then
+		if not body or body == "" then
+			local resp = "Invalid request"
+			return {code = 400, {"Content-Length", resp:len()}}, resp
+		end
+		local q = lu.parseArgs(body)
+		-- Has to be caps for some reason.
+		if q and q.transactionId and string.upper(q.transactionId) == "YES" then
+			---@type string
+			---@diagnostic disable-next-line: assign-type-mismatch
+			local resp = json.encode({
+				message = "henlo :3",
+				status = "OK",
+				-- TRANSACTION_VALID, TRANSACTION_INVALID, TRANSACTION_VIOLATION, TRANSACTION_BLOCKED
+				trans_status = "TRANSACTION_VALID"
+			})
+			return {
+				code = 400,
+				{"Content-Type", "application/json"},
+				{"Content-Length", resp:len()}
+			}, resp
+		else
+			---@type string
+			---@diagnostic disable-next-line: assign-type-mismatch
+			local resp = json.encode({
+				message = "henlo :3",
+				status = "OK",
+				trans_status = "TRANSACTION_INVALID"
+			})
+			return {
+				code = 400,
+				{"Content-Type", "application/json"},
+				{"Content-Length", resp:len()}
+			}, resp
+		end
 	else
-		res.statusCode = 404
+		local resp = "404 Not Found"
+		return {code = 404, {"Content-Length", resp:len()}}, resp
 	end
-	return true
-end}
+end}}
--- scripts/mapsx.lua
+++ scripts/mapsx.lua
@@ -3,16 +3,15 @@
 local append = "&accessKey=1771263947_2253116135311925630_%2F_oCF9gr1p%2BmhfpmMi%2BafsBlyTJysoz%2Byp%2FHBCt5rbE00%3D"
 ---@param req ch-request
 local f = function(req, go)
-	print(req.url)
-	req.url = req.url..append
-	print(req.url)
+	print(req.path)
+	req.path = req.path..append
+	print(req.path)
 end
 
--- catch-if-domain-match.
-return nil, {
+return {nil, {
 	["gsp35.ls.apple.com"] = f,
 	["gsp19-ms12.ls.apple.com"] = f,
 	["gsp12-ms12.ls.apple.com"] = f,
 	["gsp11.ls.apple.com"] = f,
 	["gsp21.ls.apple.com"] = f
-}
+}}
